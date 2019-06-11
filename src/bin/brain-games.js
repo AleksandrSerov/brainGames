5ee54e0b2791fb96d46563bedaefc46b7ac4fd70
Aleksandr Serov <sashaserov2010@gmail.com>
2019-06-11 08:39:10 +0300

    Add linter

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/local/bin/node
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
-const name = readlineSync.question("May I have your name? ");
+const name = readlineSync.question('May I have your name? ');
 
 console.log(`Hello, ${name}!`);
