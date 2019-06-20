05d601cc42aedd9c72303e7d52a642016c063fc2
Aleksandr Serov <sashaserov2010@gmail.com>
2019-06-20 16:58:42 +0300

    Refactor const

--- src/games/brainProgression.js
+++ src/games/brainProgression.js
@@ -3,7 +3,7 @@ import startGameProcessing from '../engine';
 import getRandomIntNumber from '../generator';
 
 const gameDescription = 'What number is missing in the progression?';
-const ListLength = 10;
+const listLength = 10;
 const countRounds = 3;
 
 const getProgression = (step, start, count) => {
@@ -17,8 +17,8 @@ const getProgression = (step, start, count) => {
 const getRoundData = () => {
   const step = getRandomIntNumber(1, 15);
   const start = getRandomIntNumber(0, 100);
-  const list = getProgression(step, start, ListLength);
-  const correctAnswerIndex = getRandomIntNumber(0, ListLength - 1);
+  const list = getProgression(step, start, listLength);
+  const correctAnswerIndex = getRandomIntNumber(0, listLength - 1);
   const correctAnswer = list[correctAnswerIndex];
 
   const question = `${list.slice(0, correctAnswerIndex)} .. ${list.slice(
